f31b5f01a47b60b304947174d38342a3fc359c2d
Cleilson <marcialwushu@gmail.com>
2020-04-21 23:20:51 -0300

    mav bar style header add

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">MyCopmany</a>
+        </div>
+
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="faq" class="nav-item r-item">Feature</router-link>
+          <router-link to="faq" class="nav-item r-item">About</router-link>
+          <router-link to="faq" class="nav-item r-item">FAQ</router-link>
+
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
