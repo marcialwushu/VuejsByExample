6c7d64577c6078c52d44cf2ef1597b50e2e569af
Cleilson <marcialwushu@gmail.com>
2020-04-24 20:56:27 -0300

    hero section add

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-      Home
+      <section class="hero">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title"> {{heading}} </h1>
+            <div class="is-two-thirds column is-paddingless">
+              <h2 class="subtitle is-4">{{ subheading }}</h2>
+            </div>
+            <a class="button is-large is-primary" id="learn">Learn More</a>
+          </div>
+        </div>
+      </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra felis eget maximus tincidunt.  '
+    }
+  }
 
 }
 </script>
@@ -14,5 +30,21 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import '../mq.sass'
+
+
+.hero
+  background: url('../assets/clouds.jpg')
+  background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+h2
+    margin: 1.5rem 02rem 0 !important
 
 </style>
