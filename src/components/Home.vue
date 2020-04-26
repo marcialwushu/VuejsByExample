3d993aaa0f39e709303ff5e8bd840d1d4c2612e3
Cleilson <marcialwushu@gmail.com>
2020-04-26 02:08:43 -0300

    home content cards add

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,49 @@
           </div>
         </div>
       </section>
+
+      <section class="section">
+        <div class="container">
+          <div class="columns pd is-desktop">
+            <div class="column is-1 has-text-centered">
+              <i class="fa fa-cog is-primary"></i>
+            </div>
+            <div class="column is-one-third-desktop">
+              <p class="title"><strong>We provide superiorlogistics so that your business can succeed in a crazy world.</strong></p>
+            </div>
+            <div class="column ">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra felis eget maximus tincidunt.Phasellus vitae dolor nec neque vehicula luctus eu eu nisi.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="columns pd">
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this</p>
+                <p class="subtitle">- Gary Simon</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this</p>
+                <p class="subtitle">- Gary Simon</p>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-content">
+                <p class="title">I think it's an absolutely excellent tool for our business. I can't survive without this</p>
+                <p class="subtitle">- Gary Simon</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
   </div>
 </template>
 
